c13de8b484c8c98bc6a02c323e9abb3a377b59ec
juuunior.net@gmail.com <juuunior.net@gmail.com@2f71bee6-9c02-917f-4985-20304c74be6b>
2013-02-10 17:59:35 +0000

    Продолжаем уже на MvcApplication

---
+++ trunk/Troitsa-Reutov/TroitsaReutov/js/cufon-replace.js
@@ -0,0 +1,4 @@
+Cufon.replace('#menu a', { fontFamily: 'CyrillicOld', hover:true, color: '-linear-gradient(#fdfdfd, #dedede, #b4b4b4)' });
+Cufon.replace('h2, .button, .list2 a, #sign_up a, h3', { fontFamily: 'CyrillicOld', hover:true });
+Cufon.replace('h1', { fontFamily: 'CyrillicOld', hover:true });
+
