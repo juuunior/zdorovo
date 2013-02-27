67285d05987ddf49b511c7113fdd4c8f23c07eb3
juuunior.net@gmail.com <juuunior.net@gmail.com@2f71bee6-9c02-917f-4985-20304c74be6b>
2013-02-27 11:35:00 +0000

    Теперь есть отправка писем

--- Troitsa-Reutov/TroitsaReutov/Controllers/HomeController.cs
+++ Troitsa-Reutov/TroitsaReutov/Controllers/HomeController.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
+using System.Net.Mail;
 using System.Web.Mvc;
 
 namespace TroitsaReutov.Controllers
@@ -25,6 +27,26 @@ namespace TroitsaReutov.Controllers
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
+
+			
+			if (!String.IsNullOrEmpty(Request["SenderName"]) && !String.IsNullOrEmpty(Request["SenderEmail"]) && !String.IsNullOrEmpty(Request["SenderMessage"]))
+			{
+				ViewBag.Message = "Your contact page.";
+
+				System.Net.Mail.SmtpClient client = new System.Net.Mail.SmtpClient("smtp.mail.ru", 25);
+				
+				client.Credentials= new NetworkCredential("juuunior@inbox.ru", "victory2000");
+
+				MailMessage Message = new MailMessage();
+				Message.From = new MailAddress("juuunior@inbox.ru");
+				Message.To.Add(new MailAddress("juuunior@mail.ru"));
+				Message.Subject = "Сообщение с сайта Троицкого храма от: " + Request["SenderName"];
+				Message.Body = Request["SenderMessage"] + Request["SenderEmail"];
+
+				client.Send(Message);
+				client.Dispose();
+			}
+
 
             return View();
         }
